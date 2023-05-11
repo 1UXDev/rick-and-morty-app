b3216a7206655ef67d3c99ee3069ffebbf1d4232
mntzd <julien.leske@gmail.com>
2023-05-11 20:04:28 +0200

    index.js adapted to requirements

--- components/search-bar/search-bar.js
+++ components/search-bar/search-bar.js
@@ -1,20 +1,24 @@
-// import { hiFunct } from "../card/card";
-// console.log("running")
-// export let SearchQuery = "";
+/* import { myFunction } from "../card/card.js";
+console.log("running");
+export let SearchQuery = "";
 
-// export function SearchBar() {
- 
-//   const searchBarContainer = document.querySelector(
-//     '[data-js="search-bar-container"]'
-//   );
-//   const searchBarForm = document.querySelector(`[data-js="search-bar"]`);
+export function SearchBar() {
+  const searchBarContainer = document.querySelector(
+    '[data-js="search-bar-container"]'
+  );
+  const searchBarForm = document.querySelector(`[data-js="search-bar"]`);
 
-//   searchBarForm.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     SearchQuery = searchBarForm.firstChild.nextElementSibling.value;
+  searchBarForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    SearchQuery = searchBarForm.firstChild.nextElementSibling.value;
+    return;
+  });
 
-
-//     hiFunct()
-//     return;
-//   });
-// }
+  searchBarForm.addEventListener("input", (e) => {
+    e.preventDefault();
+    SearchQuery = searchBarForm.firstChild.nextElementSibling.value;
+    myFunction(SearchQuery);
+    return;
+  });
+}
+ */
