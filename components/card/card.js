56a2bf343a03d29e3be056e0abb40699e1469f43
samuellmfa <samuellmfa@gmail.com>
2023-05-11 07:59:14 +0200

    Added fetch() and all data can be accessed dynamically

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,23 @@
-export function createCharacterCard() {}
+export async function createCharacterCard(_navigate_number = 22) {
+    const cardContainer = document.querySelector('[data-js="card-container"]');
+    const card__image = document.querySelector('[data-js="card__image"]');
+    const card__title = document.querySelector('[data-js="card__title"]');
+    const card__info_status = document.querySelector('[data-js="card__info_status"]');
+    const card__info_type = document.querySelector('[data-js="card__info_type"]');
+    const card__info_Occurrences = document.querySelector('[data-js="card__info_Occurrences"]');
+
+   const response = await fetch("https://rickandmortyapi.com/api/character/"+ _navigate_number);
+   const jsonData =  await response.json();
+ 
+   card__title.innerHTML = jsonData.name
+   card__info_status.innerHTML = jsonData.status
+   card__info_type.innerHTML = jsonData.type
+   card__info_Occurrences.innerHTML = jsonData.species
+
+
+    card__image.setAttribute('src', 'https://rickandmortyapi.com/api/character/avatar/'+_navigate_number+'.jpeg');
+    card__image.setAttribute('alt', 'Rick Sffsanchez');
+    card__image.setAttribute('data-js', 'card__image');
+    card__image.setAttribute('class', 'card__image');
+
+}
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+import { createCharacterCard } from "./components/card/card.js";
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
-);
+const searchBarContainer = document.querySelector('[data-js="search-bar-container"]');
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
+
+
+
+createCharacterCard(22);
 
 // States
 const maxPage = 1;
